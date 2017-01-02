ddf9d5a2b805932477daa437c943a7158393a07e
AtsushiSakai <asakai.amsl+github@gmail.com>
2017-01-02 20:34:35 +0000

    add fail test

--- test.sh
+++ test.sh
@@ -7,7 +7,8 @@ function compile(){
   #コンパイルチェック
   if [ $? -ne 0 ]; then
     echo "Failed to comile $1"
-    exit 1 #fail
+    result=1
+    return
   fi
 
   #tmp.outという実行ファイルを作る driver.cとtmp.cを使って
@@ -15,10 +16,31 @@ function compile(){
 
   if [ $? -ne 0 ]; then
     echo "GCC failed"
+    result=1
+    return
+  fi
+
+}
+
+function fail_test(){
+  expected="$1"
+  exr="$2"
+  echo "fail_test $1 $2"
+  result=0
+  compile "$exr"
+
+  rlt="`./tmp.out`" #実行ファイルを実行して、標準出力を取得
+
+  if [ "$result" != 0 ]; then
+    echo "OK"
+  else
+    echo "Test passed but fail is extected .."
     exit 1 #fail
   fi
 
 }
+
+
 
 function test(){
   expected="$1"
@@ -58,5 +80,11 @@ test 5 '1 + 4'
 test 2 '3- 1'
 test 8 '5 +3'
 
+fail_test '"abc'
+fail_test '0abc'
+fail_test '1+'
+
 # rm -f tmp.out tmp.s
+echo "=================="
 echo "All tests passed"
+echo "=================="
